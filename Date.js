9755af934ca5673564b925de1130cd277650d7e6
Amanda Woods <Amanda@Amandas-MBP.lan1>
2019-07-05 16:18:32 -0700

    Links to styled questionnaire and User class created

---
+++ Date.js
@@ -0,0 +1,69 @@
+class User{
+  constructor(name, sex, race, preferredSex, preferredRace){
+    this.name = name;
+    this.sex = sex;
+    this.race = race;
+    this.preferredSex = preferredSex;
+    this.preferredRace = preferredRace;
+  }
+  get name(){
+    return this.name;
+  }
+  get sex(){
+    return this.sex;
+  }
+  get race(){
+    return this.race;
+  }
+  get preferredSex(){
+    return this.preferredSex;
+  }
+  get preferredRace(){
+    return this.preferredRace;
+  }
+  set name(){
+    let userName = prompt('Name');
+    if(typeof(userName)!== 'string'){
+      alert('Error: Name must be a string');
+    }
+    else {
+      this.name = userName;
+    }
+  }
+  set sex(){
+    let userSex = prompt('Sex');
+    if(userSex == 'Male' || 'Female'){
+      this.sex = userSex;
+    }
+    else {
+      alert('Error: Please state whether you are male or female');
+    }
+  }
+  set race(){
+    let userRace = prompt('Race');
+    if(typeof(userRace)!== 'string'){
+      alert('Error: Race must be a string');
+    }
+    else{
+      this.race = userRace;
+    }
+  }
+  set preferredSex(){
+    let userPreferredSex = prompt('Are you seeking a man or a woman?');
+    if (userPreferredSex == 'Man' || 'Woman'){
+      this.preferredSex = userPreferredSex;
+    }
+    else {
+      alert('Please state whether you are seeking a man or a woman');
+    }
+  }
+  set preferredRace(){
+    let userPreferredRace = prompt('Do you prefer a specific race(s)? If no preference please leave blank');
+    if (typeof(userPreferredRace) == 'string' || 'null'){
+      this.preferredRace = userPreferredRace;
+    }
+    else {
+      alert('If you have no preference please leave this field blank');
+    }
+  }
+}
